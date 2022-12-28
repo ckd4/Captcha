1552d5233b5710bfc4f26cb8c98a3572b09a7f6c
ckd4 <107938367+ckd4@users.noreply.github.com>
2022-12-28 05:49:36 +0300

    Add files via upload

---
+++ Captcha/CaptchaClass/CaptchaC.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptchaClass
+{
+    public class CaptchaC
+    {
+        public string Value { get; set; }
+        public uint SymbolsCount { get; set; }
+        public uint DigitsCount { get; set; }
+        public bool Repeat { get; set; }
+
+        public CaptchaC(uint SymbolsCount, uint DigitsCount, bool Repeat)
+        {
+            //if (DigitsCount > SymbolsCount) throw new Exception("Количество чисел больше числа символов");
+
+            this.SymbolsCount = SymbolsCount;
+            this.DigitsCount = DigitsCount;
+            this.Repeat = Repeat;
+        }
+        public void Generate()
+        {
+            string password = "";
+            Random rnd = new Random();
+            for (int i = 0; i < DigitsCount; i++)
+            {
+                int a = rnd.Next(0, 2);
+                if (a == 0)
+                    password += Convert.ToChar(rnd.Next(65, 90));
+                else
+                    password += Convert.ToChar(rnd.Next(97, 122));
+            }
+                Value = password;
+/*        {
+            List<char> chars = new List<char>("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            char[] captcha = new char[SymbolsCount];
+            int used = 0;
+            Random r = new Random();
+            for (int i = 0; i < SymbolsCount; i++)
+            {
+                if (DigitsCount < used && Convert.ToBoolean(r.Next(0, 1)))
+                {
+                    captcha[i] = Convert.ToChar(r.Next(0, 9));
+                    used++;
+                }
+                else
+                {
+                    captcha[i] = chars[r.Next(0, chars.Count)];
+                    if (!Repeat) chars.Remove(captcha[i]);
+                }
+            }
+            Value = String.Join("", chars);
+        }
+        public bool Compare(string captcha)
+        {
+            return captcha == Value;
+        }*/
+        }
+    }
+}
---
+++ Captcha/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CaptchaClass;
+
+namespace Captcha
+{
+    public partial class MainWindow : Window
+    {
+        CaptchaC C;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateBT_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                C = new CaptchaC(Convert.ToUInt32(SymbolsTB.Text), Convert.ToUInt32(DigitsTB.Text), Convert.ToBoolean(RepeatCB.IsChecked));
+                C.Generate();
+                CaptchaLabel.Content = C.Value;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        private void CheckBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (CaptchaLabel.Content.ToString() == CheckTB.Text)
+                LabelValid.Content = "Captcha is valid";
+            else
+                LabelValid.Content = "Captcha is not valid!";
+        }
+    }
+}
